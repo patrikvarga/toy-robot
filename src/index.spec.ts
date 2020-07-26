3fc5889a6ee1150964763e9623a066af7b2f0acd
Patrik Varga <varga.patrik@gmail.com>
2020-07-26 11:08:51 +1000

    Add ts/node project skeleton with working test watch

---
+++ src/index.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+
+describe("Toy Robot Simulator", () => {
+    beforeEach(() => {
+    });
+
+    it("works", () => {
+        expect("volkswagen").to.eq("volkswagen");
+    });
+});
