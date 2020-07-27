c7d89d9381752da295048af0340cebfabc704b83
Patrik Varga <varga.patrik@gmail.com>
2020-07-27 12:16:13 +1000

    Support file input

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import * as process from "process"
+import { createReadStream } from "fs"
 import { createInterface } from "readline"
 import Robot from "./robot"
 import Table from "./table"
 import Command from "./command"
 
+const inputStream = process.argv[2] ? createReadStream(process.argv[2]) : process.stdin
+
 const cli = createInterface({
-  input: process.stdin,
+  input: inputStream,
   output: process.stdout,
   terminal: false
 })
